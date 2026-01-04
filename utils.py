a1fecbfd3765a02a34cd9537c04ec6c717e432e4
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-01-04 09:29:47 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,24 +1,23 @@
 import logging
+import os
 from logging.handlers import RotatingFileHandler
+
+handlers = [logging.StreamHandler()]
+
+if os.getenv("VERCEL") is None:
+    handlers.append(
+        RotatingFileHandler(
+            "APILOG.txt",
+            maxBytes=50000000,
+            backupCount=10
+        )
+    )
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
-    datefmt='%Y-%m-%d %H:%M:%S',
-    handlers=[
-        RotatingFileHandler(
-            "botlog.txt",
-            maxBytes=50000000,
-            backupCount=10
-        ),
-        logging.StreamHandler()
-    ]
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=handlers
 )
 
-logging.getLogger("aiogram").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("telethon").setLevel(logging.ERROR)
-logging.getLogger("aiohttp").setLevel(logging.ERROR)  
-logging.getLogger("apscheduler").setLevel(logging.ERROR)
-
-LOGGER = logging.getLogger(__name__)
+LOGGER = logging.getLogger("FILETOLINK")
